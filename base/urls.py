a51d842e68259b994137a38b201e7af642d3d203
Invisible <vitaliysomoniuk@gmail.com>
2022-01-19 20:06:55 +0200

    Added login & logout user

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,11 @@
+from unicodedata import name
 from django.urls import path
-from .views import  taskList, taskDetail, taskCreate, taskUpdate, taskDelete
+from .views import  taskList, taskDetail, taskCreate, taskUpdate, taskDelete, customLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', customLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', taskList.as_view(), name='tasks'),
     path('task/<int:pk>/', taskDetail.as_view(), name='task'),
     path('task-create/', taskCreate.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -1,12 +1,21 @@
 from re import T
 from statistics import mode
-from django.forms import DateField, models
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from .models import Task
+
+
+class customLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_auhenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 
 class taskList(ListView):
