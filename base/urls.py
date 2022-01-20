3eecfbb91c167764cdb1d69392d435827399056a
Invisible <vitaliysomoniuk@gmail.com>
2022-01-20 20:22:53 +0200

    Added restricted pages and register page

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,13 @@
 from unicodedata import name
 from django.urls import path
-from .views import  taskList, taskDetail, taskCreate, taskUpdate, taskDelete, customLoginView
+from .views import  taskList, taskDetail, taskCreate, taskUpdate, taskDelete, customLoginView, registerPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', customLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', registerPage.as_view(), name='register'),
+
     path('', taskList.as_view(), name='tasks'),
     path('task/<int:pk>/', taskDetail.as_view(), name='task'),
     path('task-create/', taskCreate.as_view(), name='task-create'),
